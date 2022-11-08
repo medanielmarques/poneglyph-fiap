d3c48ca43dcee18a51d9bdeff78d789f344bc531
Daniel Marques <daniel.brz2009@gmail.com>
2022-11-08 03:06:41 -0300

    wip

--- src/pages/new-profile/index.page.tsx
+++ src/pages/new-profile/index.page.tsx
@@ -1,4 +1,11 @@
-import { FaCog, FaShareAlt } from 'react-icons/fa';
+import {
+  FaBolt,
+  FaCheckCircle,
+  FaCog,
+  FaFire,
+  FaShareAlt,
+  FaTrophy,
+} from 'react-icons/fa';
 
 const luffy =
   'http://pm1.narvii.com/7857/96c0736f007f9dff413d02504db056fcd81db20ar1-1080-1080v2_00.jpg';
@@ -27,14 +34,51 @@ export default function Profile() {
         <span className='text-lg font-medium'>Estatísticas</span>
       </div>
 
-      <div className='flex flex-col'>
-        <div className='z-10 -mb-4 ml-4 w-8 h-8 rounded-xl bg-orange-400 flex justify-center items-center'>
-          IK
+      <div className='flex gap-6'>
+        <div className='flex flex-col'>
+          <div className='text-white z-10 -mb-5 ml-6 w-10 h-10 rounded-xl bg-orange-400 flex justify-center items-center'>
+            <FaFire size={23} />
+          </div>
+
+          <div className='relative overflow-hidden flex flex-col gap-7 w-40 h-44 bg-gray-100 px-5 pt-7 rounded-3xl'>
+            <span className='font-semibold text-lg'>Dias seguidos</span>
+            <span className='font-bold text-5xl'>56</span>
+          </div>
         </div>
 
-        <div className='relative overflow-hidden flex flex-col gap-7 w-40 h-44 bg-gray-100 px-5 pt-7 rounded-3xl'>
-          <span className='font-semibold text-lg'>Dias seguidos</span>
-          <span className='font-bold text-5xl'>56</span>
+        <div className='flex flex-col'>
+          <div className='z-10 -mb-5 ml-6 w-10 h-10 rounded-xl bg-orange-400 flex justify-center items-center text-white'>
+            <FaBolt size={23} />
+          </div>
+
+          <div className='relative overflow-hidden flex flex-col gap-7 w-40 h-44 bg-gray-100 px-5 pt-7 rounded-3xl'>
+            <span className='font-semibold text-lg'>XP</span>
+            <span className='font-bold text-5xl'>8000</span>
+          </div>
+        </div>
+      </div>
+
+      <div className='flex gap-6 mt-6'>
+        <div className='flex flex-col'>
+          <div className='text-white z-10 -mb-5 ml-6 w-10 h-10 rounded-xl bg-orange-400 flex justify-center items-center'>
+            <FaTrophy size={23} />
+          </div>
+
+          <div className='relative overflow-hidden flex flex-col gap-7 w-40 h-44 bg-gray-100 px-5 pt-7 rounded-3xl'>
+            <span className='font-semibold text-lg'>Ranking</span>
+            <span className='font-bold text-5xl'>3º</span>
+          </div>
+        </div>
+
+        <div className='flex flex-col'>
+          <div className='text-white z-10 -mb-5 ml-6 w-10 h-10 rounded-xl bg-orange-400 flex justify-center items-center'>
+            <FaCheckCircle size={23} />
+          </div>
+
+          <div className='relative overflow-hidden flex flex-col gap-7 w-40 h-44 bg-gray-100 px-5 pt-7 rounded-3xl'>
+            <span className='font-semibold text-lg'>Aulas feitasv</span>
+            <span className='font-bold text-5xl'>42</span>
+          </div>
         </div>
       </div>
     </div>
